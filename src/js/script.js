321132f9f722aa49bef5762d7f3eab78cb330ed5
Taras Samoilov <tarsam74@gmail.com>
2022-03-29 22:56:44 +0300

    Добавлены секции Reviews, Video, Clients. Нужно будет задать отступы между слайдами

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,24 @@ $(function () {
 		nextArrow:
 			'<button type="button" class="slick-next"><img src="icons/sliderArrowR_white.png"></button>',
 	});
+
+	$('.reviews__slider').slick({
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		prevArrow:
+			'<button type="button" class="slick-prev"><img src="icons/sliderArrowL.png"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img src="icons/sliderArrowR.png"></button>',
+	});
+
+	$('.clients__slider').slick({
+		infinite: true,
+		slidesToShow: 6,
+		slidesToScroll: 6,
+		prevArrow:
+			'<button type="button" class="slick-prev"><img src="icons/sliderArrowL.png"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img src="icons/sliderArrowR.png"></button>',
+	});
 });
