2666144ccf90e529026590f9e128a5cd21682f45
Taras Samoilov <tarsam74@gmail.com>
2022-04-11 21:28:25 +0300

    Добавлена страница Sponsors

--- src/js/script.js
+++ src/js/script.js
@@ -25,4 +25,27 @@ $(function () {
 		nextArrow:
 			'<button type="button" class="slick-next"><img src="icons/sliderArrowR.png"></button>',
 	});
+
+	// Smooth scroll and pageup
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href^='#']").click(function () {
+		const _href = $(this).attr('href');
+		$('html, body').animate({ scrollTop: $(_href).offset().top + 'px' });
+		return false;
+	});
+
+	// Modal
+	$('[data-modal=subscribe]').on('click', function () {
+		$('.overlay, #subscribe').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function () {
+		$('.overlay, #subscribe').fadeOut('slow');
+	});
 });
